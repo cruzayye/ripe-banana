e806d1543b108963ba6d965d3e162df311cbcf50
cruzayye <johnnyc010392@gmail.com>
2019-01-25 15:12:41 -0800

    created get but its not passing need to creat post

--- test/studio.test.js
+++ test/studio.test.js
@@ -6,6 +6,16 @@ const mongoose = require('mongoose');
 const app = require('../lib/app');
 
 describe('Studio app', () => {
+
+  const createStudio = (name, address) => {
+    return request(app)
+      .post('/studios')
+      .send({
+        name: name,
+        address: address
+      })
+      .then(res => console.log(res.body));
+  };
   it('validates a good model', () => {
     const studio = new Studio({
       name: 'Universal',
@@ -26,5 +36,18 @@ describe('Studio app', () => {
       _id: expect.any(mongoose.Types.ObjectId)
      
     });
+  });
+
+  it('gets list of studios', () => {
+    return Promise.all(['Universal', 'Fox', 'Disney'].map(studio => {
+      createStudio(studio);
+    }))
+      .then(() => {
+        return request(app)
+          .get('/studios');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(3);
+      });
   });
 });
