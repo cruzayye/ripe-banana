e10e076734e7dd48854077f47021a28ddd52bb9a
Carmen V Ramos <carmenvramos@yahoo.com>
2019-02-01 13:26:50 -0800

    Film Model test completed and passing

--- test/Unit/Film.test.js
+++ test/Unit/Film.test.js
@@ -1,37 +1,58 @@
 require('dotenv').config();
-require('../../lib/utils/connect');
+require('../../lib/utils/connect')();
 
 const Film = require('../../lib/models/Film');
+const Studio = require('../../lib/models/Studio');
+const Actor = require('../../lib/models/Actor');
 const mongoose = require('mongoose');
 
 describe('Film Model Test', () => {
 
   beforeEach(done => {
-    return mongoose.connection.dropDatabase(() => {
-      done();
-    });
+    mongoose.connection.dropDatabase(done);
   });
 
   it('validates a good model', () => {
+    const studio = new Studio({
+      name: 'Universal',
+      address: {
+        city: 'Los Angeles',
+        state: 'CA',
+        country: 'USA'
+      }
+    });
+
+    const actor = new Actor({
+      name: 'Keanu Reeves',
+      dob: '1970-01-01',
+      pob: 'Beverly Hills'
+    });
+
     const film = new Film({
       title: 'The Matrix',
-      studio: mongoose.Types.ObjectId(),
+      studio: studio._id,
       released: 1994,
       cast: [{
         role: 'Neo',
-        actor: mongoose.Types.ObjectId()
+        actor: actor._id,
       }]
     });
-    expect(film.toJSON).toEqual({
+    expect(film.toJSON()).toEqual({
       _id: expect.any(mongoose.Types.ObjectId),
       title: 'The Matrix',
-      studio: mongoose.Types.ObjectId(),
+      studio: expect.any(mongoose.Types.ObjectId),
       released: 1994,
       cast: [{
+        _id: expect.any(mongoose.Types.ObjectId),
         role: 'Neo',
-        actor: mongoose.Types.ObjectId()
+        actor: expect.any(mongoose.Types.ObjectId)
       }]
     });
   }); 
+
+  afterAll((done) => {
+    mongoose.disconnect(done);
+  }); 
+
 });
 
