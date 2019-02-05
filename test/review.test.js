2da20777f7a519b211652e4cecb13df3df9c2092
cruzayye <johnnyc010392@gmail.com>
2019-02-04 18:24:37 -0800

    save before carmens final pull request

--- test/review.test.js
+++ test/review.test.js
@@ -14,15 +14,33 @@ describe('Review tests', () => {
       .then(reviewer => ({ ...reviewer, _id: reviewer._id.toString() })); 
   });
   
-  const createFilm = ((title, studio, released, cast) => {
-    return Film.create({ title, studio, released, cast })
+  const createFilm = ((title, studio, released) => {
+    return Film.create({ title, studio, released })
       .then(film => ({ ...film, _id: film._id.toString() }));
   });
 
-  const CreateReview = (( review, rating, reviewer, film, createdAt, updatedAt) => {
-    return
-  
-  })
+
+  const CreateReview = ((review, rating, reviewer, film, createdAt, updatedAt) => {
+    const ReviewerOjb = createReviewer('johnny', 'productions');
+    const filmObj = createFilm('Titanic', 'Universal', '10-12-1996');
+    return Promise.all([ Promise.resolve(ReviewerOjb, filmObj)])
+    // return Promise.all([createReviewer('johnny', 'jcProductions'), createFilm('Titanic', 'universal', '10-10-1996')])
+      
+  });
+  // const CreateReview = (( review, rating, reviewer, film, createdAt, updatedAt) => {
+  //   return createReviewer('johnny', 'jcProductions')
+  //     .then(() => {
+  //       return createFilm( 'Titanic', 'Universal', '10-12-1996', )
+  //     })
+
+  // })
+  return createActor('Brad Pitt')
+  .then(createdActor => {
+    return Promise.all([
+      Promise.resolve(createdActor.id),
+      request(app)
+        .get(`/actors/${createdActor._id}`)
+    ])
    
   beforeEach(done => {
     mongoose.connection.dropDatabase(done);
